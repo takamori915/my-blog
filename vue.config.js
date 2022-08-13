dba7b624c660142076826a0783c78ffc78b1c8f6
takamori <takamori915@gmail.com>
2022-08-14 06:13:24 +0900

    production用の設定を追加（github公開用）

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/my-blog' : '/',
+  outputPath: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false, 
 }
