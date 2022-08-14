c46fa3014867cb8e91916900f85fe840a97763b0
takamori <takamori915@gmail.com>
2022-08-14 22:41:45 +0900

    router-viewを追加

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,9 @@
           <router-link to="/about">About</router-link>
         </div>
       </v-app-bar>
+      <router-view/>
     </v-container>
   </v-app>
-  <!-- <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div> -->
 </template>
 
 <style lang="scss">
